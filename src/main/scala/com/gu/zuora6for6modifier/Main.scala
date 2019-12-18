34d2c6e200a80f0669b59b4f4054f0c959822fe2
Kelvin Chappell <kelvin.chappell@theguardian.com>
2019-12-18 14:57:41 +0000

    A bit more explanation

--- src/main/scala/com/gu/zuora6for6modifier/Main.scala
+++ src/main/scala/com/gu/zuora6for6modifier/Main.scala
@@ -60,8 +60,12 @@ object Main extends App {
 
   /**
     * @param args  valid values of arg(0) are:<ul>
-    *              <li>"extend" to extend the 6-for-6 period so that it includes at least 6 issues</li>
-    *              <li>"postpone" to postpone the start of the 6-for-6 period by a week</li></ul>
+    *              <li>"extend" to extend the 6-for-6 period so that it includes at least 6 issues.<br />
+    *               In this case a file called "extend.in.txt" will be expected in the root
+    *               of the project holding a list of subscription names, one on each line.</li>
+    *              <li>"postpone" to postpone the start of the 6-for-6 period by a week<br />
+    *               In this case a file called "postpone.in.txt" will be expected in the root
+    *               of the project holding a list of subscription names, one on each line.</li></ul>
     *              In both cases the start of the subsequent quarterly plan is postponed
     *              until the new end of the introductory plan.
     */
